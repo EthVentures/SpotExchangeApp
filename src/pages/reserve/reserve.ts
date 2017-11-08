9593a11cb351d336884ba5fffa24c87726083e59
John O'Sullivan <josullivan1@luc.edu>
2017-11-07 21:13:08 -0600

    Fixed rendering issue

--- src/pages/reserve/reserve.ts
+++ src/pages/reserve/reserve.ts
@@ -42,6 +42,7 @@ export class ReservePage {
         set: false,
     };
     placesService:any;
+    interval:any;
 
     constructor(public modalCtrl: ModalController,public authService:AuthService,public navCtrl: NavController, public geolocation: Geolocation, public mockdata:MockDataService,public appConfig:AppConfig) {
       this.markers = [];
@@ -49,6 +50,7 @@ export class ReservePage {
       //console.log(this.polys);
       this.searchQuery = '';
       this.isRunningGEO = false;
+      this.interval = null;
     }
 
     search(event) {
@@ -56,9 +58,75 @@ export class ReservePage {
     }
 
     showModal() {
-        // reset
         this.reset();
         let modal = this.modalCtrl.create(ModalAutocompleteItems);
+        var self = this;
+        this.interval = setInterval(function(){
+          //console.log("Waiting");
+          if (self.address.set) {
+            clearInterval(self.interval);
+            self.interval = null;
+            self.lat = self.placedetails['lat'];
+            self.lng = self.placedetails['lng'];
+
+            self.markers = [];
+
+            self.mockdata.parkwhizSpot(self.lat, self.lng).subscribe(data => {
+
+                for (var i = 0; i < data.length; i++) {
+                  var base = data[i];
+                  var item = base._embedded['pw:location'];
+                  var location = item.entrances[0].coordinates;
+
+                  var price = 0;
+                  var purchase = base.purchase_options;
+                  if (purchase.length > 0) {
+                    price = parseFloat(purchase[0].price.USD);
+                  }
+
+                  var itemLatLng = {lat: location[0], lng: location[1], icon:'assets/images/theirs.png',rate:price, address:item.address1, name:item.name };
+                  itemLatLng['covered'] = false;
+                  itemLatLng['valet'] = false;
+                  itemLatLng['covered'] = false;
+                  itemLatLng['selfPark'] = false;
+                  itemLatLng['inOutAllowed'] = false;
+                  itemLatLng['handicapAccessible'] = false;
+                  self.markers.push(itemLatLng);
+                }
+                self.loaded = true;
+            });
+
+            if (self.appConfig.NO_FAKE_SWITCH) {
+
+            self.authService.getSpots().subscribe(
+              data => {
+                if (data.success) {
+                  var spots = data.spots;
+                  for (var i = 0; i < spots.length; i++) {
+                    var cords = spots[i];
+                    var item = JSON.parse(cords.coordinates);
+                    item['icon'] = 'assets/images/ours.png';
+                    item['name'] = '';
+                    item['address'] = cords.address.street + ' ' + cords.address.city + ' ' + cords.address.state ;
+                    item['rate'] = cords.ratePerHour;
+                    item['valet'] = cords.features.valet;
+                    item['covered'] = cords.features.covered;
+                    item['selfPark'] = cords.features.selfPark;
+                    item['inOutAllowed'] = cords.features.inOutAllowed;
+                    item['handicapAccessible'] = cords.features.handicapAccessible;
+                    self.markers.push(item);
+                  }
+                }
+              },
+              err => {
+                console.log(err);
+              }
+            );
+
+            }
+          }
+        }, 1000);
+
         modal.onDidDismiss(data => {
             if(data){
                 this.searchQuery = data.description;
@@ -231,64 +299,6 @@ export class ReservePage {
       }
     }
 
-    reload() {
-      this.mockdata.parkwhizSpot(this.lat, this.lng).subscribe(data => {
-          for (var i = 0; i < data.length; i++) {
-            var base = data[i];
-            var item = base._embedded['pw:location'];
-            var location = item.entrances[0].coordinates;
-
-            var price = 0;
-            var purchase = base.purchase_options;
-            if (purchase.length > 0) {
-              price = parseFloat(purchase[0].price.USD);
-            }
-
-            var itemLatLng = {lat: location[0], lng: location[1], icon:'assets/images/theirs.png',rate:price, address:item.address1, name:item.name };
-            itemLatLng['covered'] = false;
-            itemLatLng['valet'] = false;
-            itemLatLng['covered'] = false;
-            itemLatLng['selfPark'] = false;
-            itemLatLng['inOutAllowed'] = false;
-            itemLatLng['handicapAccessible'] = false;
-            this.markers.push(itemLatLng);
-          }
-          //console.log(this.markers);
-          this.loaded = true;
-      });
-
-      if (this.appConfig.NO_FAKE_SWITCH) {
-
-      this.authService.getSpots().subscribe(
-        data => {
-          //console.log(data);
-          if (data.success) {
-            var spots = data.spots;
-            for (var i = 0; i < spots.length; i++) {
-              //console.log(spots[i]);
-              var cords = spots[i];
-              var item = JSON.parse(cords.coordinates);
-              item['icon'] = 'assets/images/ours.png';
-              item['name'] = '';
-              item['address'] = cords.address.street + ' ' + cords.address.city + ' ' + cords.address.state ;
-              item['rate'] = cords.ratePerHour;
-              item['valet'] = cords.features.valet;
-              item['covered'] = cords.features.covered;
-              item['selfPark'] = cords.features.selfPark;
-              item['inOutAllowed'] = cords.features.inOutAllowed;
-              item['handicapAccessible'] = cords.features.handicapAccessible;
-              this.markers.push(item);
-            }
-          }
-        },
-        err => {
-          console.log(err);
-        },
-        () => console.log('')
-      );
-
-      }
-    }
 
     private getPlaceDetail(place_id:string):void {
         var self = this;
@@ -314,14 +324,11 @@ export class ReservePage {
                         self.placedetails.components[addressType].long = place.address_components[i]['long_name'];
                     }
                 }
-                self.lat = self.placedetails['lat'];
-                self.lng = self.placedetails['lng'];
-                self.reload();
-            } else {
+                self.address.set = true;
+            }else{
 
             }
         }
-
     }
 
     markerClick(item) {
